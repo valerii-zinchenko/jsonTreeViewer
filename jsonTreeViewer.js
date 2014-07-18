225e08a26b7c6ae2e9ca78710f413e831213b313
valerii-zinchenko <zinchenko.valerii@gmail.com>
2014-07-18 12:33:21 +0300

    store css for tree in separate file

--- jsonTreeViewer.js
+++ jsonTreeViewer.js
@@ -1,11 +1,14 @@
 /*
- * JSON Tree Viewer
+ * Fork of JSON Tree Viewer
+ * http://github.com/valerii-zinchenko/jsonTreeViewer
+ *
+ * Original JSON Tree Viewer
  * http://github.com/summerstyle/jsonTreeViewer
  *
  * Copyright 2014 Vera Lobacheva (summerstyle.ru)
  * Released under the GPL3 (GPL3.txt)
  *
- * Sun 27 2014 20:15:00 GMT+0400
+ * 18.07.2014 12:30 GMT+0300
  */
 
 'use strict';
@@ -17,67 +20,67 @@ var jsonTreeViewer = (function() {
 			if (x === null) {
 				return 'null';
 			}
-			
+
 			switch (typeof x) {
 				case 'number':
 					return 'number';
-	
+
 				case 'string':
 					return 'string';
-	
+
 				case 'boolean':
 					return 'boolean';
 			}
-	
+
 			switch(Object.prototype.toString.call(x)) {
 				case '[object Array]':
 					return 'array';
-	
+
 				case '[object Object]':
 					return 'object';
 			}
-			
+
 			throw new Error('Bad type');
 		},
-		
+
 		foreach : function(obj, func) {
 			var type = utils.get_type(obj),
 				is_last = false,
 				last, i, c;
-	
+
 			switch (type) {
 				case 'array':
 					last = obj.length - 1;
-					
+
 					for (i = 0, c = obj.length; i < c; i++) {
 						if (i === last) {
 							is_last = true;
 						}
-						
+
 						func(obj[i], i, is_last);
 					}
-	
+
 					break;
-	
+
 				case 'object':
 					var keys = Object.keys(obj);
-					
+
 					last = keys.length - 1;
-					
+
 					for (i = 0, c = keys.length; i < c; i++) {
 						if (i === last) {
 							is_last = true;
 						}
-						
+
 						func(obj[keys[i]], keys[i], is_last);
 					}
-	
+
 					break;
 			}
 		},
 		inherits : (function() {
 			var F = function() {};
-			
+
 			return function(Child, Parent) {
 				F.prototype = Parent.prototype;
 				Child.prototype = new F();
@@ -85,35 +88,35 @@ var jsonTreeViewer = (function() {
 			}
 		})()
 	};
-	
+
 	/* Node's factory */
 	function Node(name, node, is_last) {
 		var type = utils.get_type(node);
-	
+
 		switch (type) {
 			case 'boolean':
 				return new Node_boolean(name, node, is_last);
-	
+
 			case 'number':
 				return new Node_number(name, node, is_last);
-	
+
 			case 'string':
 				return new Node_string(name, node, is_last);
-	
+
 			case 'null':
 				return new Node_null(name, node, is_last);
-	
+
 			case 'object':
 				return new Node_object(name, node, is_last);
-	
+
 			case 'array':
 				return new Node_array(name, node, is_last);
-	
+
 			default:
 				throw new Error('Bad type');
 		}
 	}
-	
+
 	/* Simple type's constructor (string, number, boolean, null) */
 	function Node_simple(name, value, is_last) {
 		var self     = this,
@@ -126,49 +129,49 @@ var jsonTreeViewer = (function() {
 					<span class="value">' +
 						value +
 					'</span>';
-					
+
 				if (!is_last) {
 					str += ',';
 				}
-					
+
 				return str;
 			};
-	
+
 		el.classList.add('node');
 		el.classList.add(this.type);
 		el.innerHTML = template(name, value);
-		
+
 		self.el = el;
 	}
-	
+
 	/* Boolean */
 	function Node_boolean(name, value, is_last) {
 		this.type = "boolean";
-		
+
 		Node_simple.call(this, name, value, is_last);
 	}
-	
+
 	/* Number */
 	function Node_number(name, value, is_last) {
 		this.type = "number";
-		
+
 		Node_simple.call(this, name, value, is_last);
 	}
-	
+
 	/* String */
 	function Node_string(name, value, is_last) {
 		this.type = "string";
-		
+
 		Node_simple.call(this, name, '"' + value + '"', is_last);
 	}
-	
+
 	/* Null */
 	function Node_null(name, value, is_last) {
 		this.type = "null";
-		
+
 		Node_simple.call(this, name, value, is_last);
 	}
-	
+
 	/* Complex node's constructor (object, array) */
 	function Node_complex(name, value, is_last) {
 		var self     = this,
@@ -179,9 +182,9 @@ var jsonTreeViewer = (function() {
 						<b>' + sym[0] + '</b>\
 						<ul class="children"></ul>\
 						<b>' + sym[1] + '</b>'
-						+ comma +	
+						+ comma +
 					'</div>';
-				
+
 				if (name !== null) {
 					str = '<span class="name_wrapper">\
 						<span class="name">"' +
@@ -189,19 +192,19 @@ var jsonTreeViewer = (function() {
 							name +
 						'"</span> : </span>' + str;
 				}
-				
+
 				return str;
 			},
 			children_ul,
 			name_el,
 			children = [];
-	
+
 		el.classList.add('node');
 		el.classList.add(this.type);
 		el.innerHTML = template(name, self.sym);
-		
+
 		children_ul = el.querySelector('.children');
-		
+
 		if (name !== null) {
 			name_el = el.querySelector('.name');
 			name_el.addEventListener('click', function() {
@@ -212,22 +215,22 @@ var jsonTreeViewer = (function() {
 			self.is_root = true;
 			el.classList.add('expanded');
 		}
-		
+
 		self.el = el;
 		self.children = children;
 		self.children_ul = children_ul;
-	
+
 		utils.foreach(value, function(node, name, is_last) {
 			var child = new Node(name, node, is_last);
 			self.add_child(child);
 		});
-		
+
 		self.is_empty = !Boolean(children.length);
 		if (self.is_empty) {
 			el.classList.add('empty');
 		}
 	}
-	
+
 	Node_complex.prototype = {
 		constructor : Node_complex,
 		add_child : function(child) {
@@ -236,11 +239,11 @@ var jsonTreeViewer = (function() {
 		},
 		expand : function(is_recursive){
 			var children = this.children;
-			
+
 			if (!this.is_root) {
 				this.el.classList.add('expanded');
 			}
-			
+
 			if (is_recursive) {
 				utils.foreach(children, function(item, i) {
 					if (typeof item.expand === 'function') {
@@ -251,11 +254,11 @@ var jsonTreeViewer = (function() {
 		},
 		collapse : function(is_recursive) {
 			var children = this.children;
-			
+
 			if (!this.is_root) {
 				this.el.classList.remove('expanded');
 			}
-			
+
 			if (is_recursive) {
 				utils.foreach(children, function(item, i) {
 					if (typeof item.collapse === 'function') {
@@ -268,21 +271,21 @@ var jsonTreeViewer = (function() {
 			this.el.classList.toggle('expanded');
 		}
 	};
-	
+
 	/* Object */
 	function Node_object(name, value, is_last) {
 		this.sym = ['{', '}'];
 		this.type = "object";
-		
+
 		Node_complex.call(this, name, value, is_last);
 	}
 	utils.inherits(Node_object, Node_complex);
-	
+
 	/* Array */
 	function Node_array(name, value, is_last) {
 		this.sym = ['[', ']'];
 		this.type = "array";
-		
+
 		Node_complex.call(this, name, value, is_last);
 	}
 	utils.inherits(Node_array, Node_complex);
@@ -315,15 +318,9 @@ var jsonTreeViewer = (function() {
 
 	return {
 		parse : function(json_str, elementID) {
-			var temp;
+			var json = JSON.parse(json_str);
 
-			try {
-				temp = JSON.parse(json_str);
-			} catch(e) {
-				alert(e);
-			}
-			
-			tree.set_root(new Node(null, temp, 'last'), elementID);
+			tree.set_root(new Node(null, json, 'last'), elementID);
 
             return tree;
 		}
